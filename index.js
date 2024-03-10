e35e0349012d5853de799718640ece51e04305e7
Yusuf <145934201+YusufDev15@users.noreply.github.com>
2024-03-10 18:55:47 +0000

    articles route created, views folder created

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 import express from "express";
+import articleRouter from "./routes/articles.js";
 import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
+// Middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.send("Hello and Welcome to Yusuf's World")
-})
+// Routes
+app.use("/articles", articleRouter);
 
+// Home Route
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
+  console.log(`Server is listening on port ${port}`);
+});
