eecf77051cf9e3197d498cfcf7f8090aca20274f
Yusuf <145934201+YusufDev15@users.noreply.github.com>
2024-03-13 16:53:33 +0000

    commenting on my code to better understand

--- models/article.js
+++ models/article.js
@@ -1,17 +1,23 @@
+// Import Mongoose library
 import mongoose from "mongoose";
 
+// Define the schema for articles
 const articleSchema = new mongoose.Schema({
+  // Title of the article, required field
   title: {
     type: String,
     required: true,
   },
+  //   description of the article
   description: {
     type: String,
   },
+  //   Markdown content of the article
   markdown: {
     type: String,
     required: true,
   },
+  //   date when the article was created, defaults to current date
   created: {
     type: Date,
     default: Date.now,
--- routes/articles.js
+++ routes/articles.js
@@ -2,23 +2,25 @@ import express from "express";
 import Article from "./../models/article.js";
 const router = express.Router();
 
-// Route for new article
+// Route for creating a new article (GET request)
 router.get("/new", (req, res) => {
   res.render("articles/new.ejs");
 });
 
-// Route for viewing details 
+// Route for viewing article details (GET REQUEST)
 router.get("/:id", (req, res) => {});
 
-// POST route for creating a new article
+// Route for creating a new article (POST request)
 router.post("/", async (req, res) => {
   const article = new Article({
     title: req.body.title,
     description: req.body.description,
-    markdown: req.body.markdown
+    markdown: req.body.markdown,
   });
   try {
+    // Saving the new article to the database
     article = await article.save();
+    // redirects to the URL to view the newly created article
     res.redirect(`/articles/${article.id}`);
   } catch (err) {
     res.render("articles/new.ejs", { article: article });
